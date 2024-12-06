1b13a776cf52bd3f71c42021062b35f49b2ed3f1
Lucas <lucaswongalmeida@gmail.com>
2024-12-06 15:53:31 -0300

    3_2

--- utils.py
+++ utils.py
@@ -1,32 +1,8 @@
 import numpy as np
 import cv2
+from scipy.ndimage import convolve
 
-def global_threshold(image_array, threshold=127):
-    # Aplique o threshold a cada pixel
-    binary_image = (image_array > threshold) * 255
-    return binary_image.astype(np.uint8)
 
-def adaptive_threshold(image_array, block_size=11, C=2):
-    # Obtém as dimensões da imagem
-    height, width = image_array.shape
-
-    # Cria uma nova matriz para armazenar a imagem binária
-    binary_image = np.zeros_like(image_array)
-
-    # Define o offset para calcular a vizinhança
-    offset = block_size // 2
-
-    # Itera por cada pixel na imagem
-    for i in range(offset, height - offset):
-        for j in range(offset, width - offset):
-            # Extrai a região de vizinhança
-            neighborhood = image_array[i - offset:i + offset + 1, j - offset:j + offset + 1]
-            # Calcula o threshold como a média da vizinhança menos C
-            threshold = neighborhood.mean() - C
-            # Aplica o threshold ao pixel atual
-            binary_image[i, j] = 255 if image_array[i, j] > threshold else 0
-
-    return binary_image.astype(np.uint8)
 
 def dilation_manual(image, kernel_size=5):
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
@@ -62,7 +38,6 @@ def apply_filter(img_cv, filter_type, display_image):
     if img_cv is None:
         return
 
-    # Verifica se a imagem está em escala de cinza e converte para BGR se necessário
     if len(img_cv.shape) == 2 or img_cv.shape[2] == 1:
         img_cv = cv2.cvtColor(img_cv, cv2.COLOR_GRAY2BGR)
 
@@ -70,18 +45,32 @@ def apply_filter(img_cv, filter_type, display_image):
 
     # Filtros de Passa Baixo
     if filter_type == "low_pass_gaussian":
-        filtered_img = cv2.GaussianBlur(img_cv, (15, 15), 0)
+        kernel = np.array([[1, 4, 6, 4, 1],
+                           [4, 16, 24, 16, 4],
+                           [6, 24, 36, 24, 6],
+                           [4, 16, 24, 16, 4],
+                           [1, 4, 6, 4, 1]]) / 256.0
+        filtered_img = np.zeros_like(img_cv)
+        for i in range(3):  # Aplica o filtro a cada canal
+            filtered_img[:, :, i] = convolve(img_cv[:, :, i], kernel)
+
     elif filter_type == "low_pass_mean":
-        filtered_img = cv2.blur(img_cv, (15, 15))
+        # Kernel de Média 5x5
+        kernel = np.ones((5, 5)) / 25.0
+        filtered_img = np.zeros_like(img_cv)
+        for i in range(3):  # Aplica o filtro a cada canal
+            filtered_img[:, :, i] = convolve(img_cv[:, :, i], kernel)
+
     elif filter_type == "low_pass_median":
         filtered_img = cv2.medianBlur(img_cv, 5)
-    
+
     # Filtros de Passa Alto
     elif filter_type == "high_pass_laplacian":
         gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
         filtered = cv2.Laplacian(gray, cv2.CV_64F)
         filtered = cv2.convertScaleAbs(filtered)
         filtered_img = cv2.cvtColor(filtered, cv2.COLOR_GRAY2BGR)
+        
     elif filter_type == "high_pass_sobel":
         gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
 
@@ -107,13 +96,11 @@ def apply_filter(img_cv, filter_type, display_image):
     elif filter_type == "high_pass_roberts":
         gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
 
-        # Kernels de Roberts
         roberts_x = np.array([[1, 0],
                                [0, -1]])
         roberts_y = np.array([[0, 1],
                                [-1, 0]])
 
-        # Aplicar o filtro de Roberts
         edges_x = cv2.filter2D(gray, cv2.CV_64F, roberts_x)
         edges_y = cv2.filter2D(gray, cv2.CV_64F, roberts_y)
         filtered_img = cv2.sqrt(edges_x**2 + edges_y**2)
@@ -140,6 +127,33 @@ def apply_segmentation(img_cv, segmentation_type, display_image):
     if segmented_img is not None:
         display_image(segmented_img, modified=True)  # Exibe a imagem editada
         return segmented_img
+
+def global_threshold(image_array, threshold=127):
+    # Aplique o threshold a cada pixel
+    binary_image = (image_array > threshold) * 255
+    return binary_image.astype(np.uint8)
+
+def adaptive_threshold(image_array, block_size=11, C=2):
+    # Obtém as dimensões da imagem
+    height, width = image_array.shape
+
+    # Cria uma nova matriz para armazenar a imagem binária
+    binary_image = np.zeros_like(image_array)
+
+    # Define o offset para calcular a vizinhança
+    offset = block_size // 2
+
+    # Itera por cada pixel na imagem
+    for i in range(offset, height - offset):
+        for j in range(offset, width - offset):
+            # Extrai a região de vizinhança
+            neighborhood = image_array[i - offset:i + offset + 1, j - offset:j + offset + 1]
+            # Calcula o threshold como a média da vizinhança menos C
+            threshold = neighborhood.mean() - C
+            # Aplica o threshold ao pixel atual
+            binary_image[i, j] = 255 if image_array[i, j] > threshold else 0
+
+    return binary_image.astype(np.uint8)
 
 def apply_morphological_operation(img_cv, operation_type, display_image):
     if img_cv is None:
